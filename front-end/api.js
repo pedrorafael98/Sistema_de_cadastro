6d85afc9b960fd146800bacb90d9bbfd69935bb9
pedrorafael98 <136933047+pedrorafael98@users.noreply.github.com>
2024-12-10 17:53:24 -0300

    .

--- front-end/api.js
+++ front-end/api.js
@@ -1,8 +1,16 @@
 
-var cpf = "";
-var senha = "";
-function usuario(){
-    cpf = window.document.getElementById("login");
-    senha = window.document.getElementById("senha");
-    window.alert('deu certo')
+function usuario(request, response){
+   let cpf = window.document.getElementById("login");
+   let senha = window.document.getElementById("senha");
+   cpf = String(cpf.value);
+   senha = String(senha.value);
+    
+    response.json({
+        cpfUser: cpf,
+        senhaUser: senha,
+        
+    });
+    
 }
+
+//export default usuario;
